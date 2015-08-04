6593ef86a40654012bc3b8209e9e74bd921d2c2f
earlofyor714 <alexphillipmartinez@gmail.com>
2015-08-03 22:47:39 -0700

    Revert "added a newline"
    This reverts commit c6f7dd8406de24c4daf593cad2671ed1825545bb.

--- predict.m
+++ predict.m
@@ -24,7 +24,6 @@ p = zeros(size(X, 1), 1);
 % X dimensions = #trainers x input_layer nodes
 
 
-
 % add X0
 X = [ones(m,1) X];
 hidden_layer = sigmoid(X*Theta1');					%Theta1 dimensions = hidden_layer x (input_layer+1)
